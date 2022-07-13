f327cd159f0086f3946f5ca844854437437b60e8
BlackHawkCH91 <christian.hensman1@gmail.com>
2022-07-14 02:05:55 +1000

    Attempting to use vosk

--- TornCityBot/Program.cs
+++ TornCityBot/Program.cs
@@ -1,9 +1,11 @@
 ﻿//Libraries
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Vosk;
 
 ChromeOptions options = new ChromeOptions();
 options.AddArgument("--disable-blink-features=AutomationControlled");
+options.AddExcludedArgument("enable-automation");
 
 IWebDriver driver = new ChromeDriver(@"C:\ChromeDrivers\103\", options);
 driver.Navigate().GoToUrl("https://www.google.com");
