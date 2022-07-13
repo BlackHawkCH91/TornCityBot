1f7f8009e48657a45e4245ca773419f3f25111b8
BlackHawkCH91 <christian.hensman1@gmail.com>
2022-07-13 15:19:23 +1000

    Driver should be hidden for now. Need to remove annoying notif.

--- TornCityBot/Program.cs
+++ TornCityBot/Program.cs
@@ -1,4 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Libraries
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+ChromeOptions options = new ChromeOptions();
+options.AddArgument("--disable-blink-features=AutomationControlled");
+
+IWebDriver driver = new ChromeDriver(@"C:\ChromeDrivers\103\", options);
+driver.Navigate().GoToUrl("https://www.google.com");
+
 
 
 Console.WriteLine("Hello, World!");
