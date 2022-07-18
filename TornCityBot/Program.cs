c2c3710cafcb62c5098a564c5279bb7258ece266
BlackHawkCH91 <christian.hensman1@gmail.com>
2022-07-19 00:46:01 +1000

    Captcha solver works. Moved it to a function.

--- TornCityBot/Program.cs
+++ TornCityBot/Program.cs
@@ -7,31 +7,36 @@ using NAudio.Wave;
 using System.Diagnostics;
 using Vosk;
 using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 //testing vars
-bool enableSelenium = false;
-string RecognisedText = "test";
+bool enableSelenium = true;
 Vosk.Vosk.SetLogLevel(-1);
+//Console.WriteLine(SpeechRec());
+
+//Start selenium
+ChromeOptions options = new ChromeOptions();
+options.AddArgument("--disable-blink-features=AutomationControlled");
+options.AddExcludedArgument("enable-automation");
+options.AddArgument("--mute-audio");
+//string username = "chook";
+//string userProfile = "C:/Users/" + username + "/AppData/Local/Google/Chrome/User Data";
+//options.AddArguments("user-data-dir=" + userProfile);
+options.AddArgument("--profile-directory=" + "Profile 2");
+
+IWebDriver driver = new ChromeDriver(@"C:\ChromeDrivers\103\", options);
+driver.Manage().Window.Maximize();
 
 if (enableSelenium)
 {
-    //Start selenium
-    ChromeOptions options = new ChromeOptions();
-    options.AddArgument("--disable-blink-features=AutomationControlled");
-    options.AddExcludedArgument("enable-automation");
-    options.AddArgument("--mute-audio");
-    string username = "chook";
-    string userProfile = "C:/Users/" + username + "/AppData/Local/Google/Chrome/User Data";
-    //options.AddArguments("user-data-dir=" + userProfile);
-    options.AddArgument("--profile-directory=" + "Profile 2");
-
-    IWebDriver driver = new ChromeDriver(@"C:\ChromeDrivers\103\", options);
-    driver.Manage().Window.Maximize();
-
+    
     driver.Navigate().GoToUrl("https://www.google.com/recaptcha/api2/demo");
+    CaptchaSolver();
 
-    //Captcha solver
+}
 
+void CaptchaSolver()
+{
     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
     List<IWebElement> iFrames = driver.FindElements(By.TagName("iframe")).ToList();
 
@@ -48,27 +53,25 @@ if (enableSelenium)
     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
     //recaptcha-audio-button
 
-
     //Get link for audio and download it
     driver.SwitchTo().DefaultContent();
     iFrames = driver.FindElements(By.TagName("iframe")).ToList();
     driver.SwitchTo().Frame(iFrames[iFrames.Count - 1]);
     string audioUrl = driver.FindElement(By.Id("audio-source")).GetAttribute("src");
-
+    
     using (var client = new System.Net.WebClient())
     {
         client.DownloadFile(audioUrl, "audio.mp3");
     }
 
-    SpeechRec();
-
-    //id = audio-source
-    //title = recaptcha challenge expires in two minutes
+    driver.FindElement(By.Id("audio-response")).SendKeys(SpeechRec());
+    driver.FindElement(By.Id("audio-response")).SendKeys(Keys.Enter);
+    RandomWait(8, 12);
 }
 
 
 
-void SpeechRec()
+string SpeechRec()
 {
     int bitRate;
     //Convert mp3 to wav
@@ -114,5 +117,10 @@ void SpeechRec()
             }
         }
     }
-    Console.WriteLine(rec.FinalResult());
+    return (dynamic)JObject.Parse(rec.FinalResult())["text"];
+}
+
+void RandomWait(int a, int b)
+{
+    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(a + new Random().NextDouble() * 5);
 }
