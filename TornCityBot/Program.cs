b03abf5b29f647389115093a05576220a2451c40
BlackHawkCH91 <christian.hensman1@gmail.com>
2022-07-23 01:53:23 +1000

    Bot can log in and auto gym.

--- TornCityBot/Program.cs
+++ TornCityBot/Program.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
     
 //testing vars
-bool enableSelenium = true;
+bool headless = false;
 Vosk.Vosk.SetLogLevel(-1);
 //Console.WriteLine(SpeechRec());
 
@@ -24,14 +24,20 @@ options.AddArguments("user-data-dir=" + userProfile);
 options.AddArgument("--profile-directory=Default");
 options.AddArgument("--disable-blink-features=AutomationControlled");
 options.AddArgument("--mute-audio");
-options.AddArgument("headless");
+options.AddArgument("window-size=1920,1080");
 options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
 options.AddExcludedArgument("enable-automation");
+
+if (headless)
+{
+    options.AddArgument("headless");
+}
 
 Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
 
 using var driver = new ChromeDriver(@"C:\ChromeDrivers\103\", options);
-IDevTools devTools = driver as IDevTools;
+WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 30));
+//IDevTools devTools = driver as IDevTools;
 driver.Manage().Window.Maximize();
 Dictionary<string, object> cmdParams = new Dictionary<string, object>();
 var loadScript = @"window.chrome = " + chromeTest + @";
@@ -55,58 +61,85 @@ var loadScript = @"window.chrome = " + chromeTest + @";
                         return getParameter(parameter);
                     };";
 
-var initialScript = @"Object.defineProperty(Notification, 'permission', {
-                        get: function () { return ''; }
-                        })                                
-                        window.chrome = " + chromeTest + @"
-                        Object.defineProperty(navigator, 'webdriver', {
-                        get: () => false})  
-                        Object.defineProperty(window, 'chrome', {
-                        get: () => true})  
-                        Object.defineProperty(navigator, 'plugins', {
-                        writeable: true,
-                        configurable: true,
-                        enumerable: true,
-                        value: 'works'})                        
-                        navigator.plugins.length = 1                                
-                        Object.defineProperty(navigator, 'language', {
-                        get: () => 'el - GR'});
-                        Object.defineProperty(navigator, 'deviceMemory', {
-                        get: () => 8});
-                        Object.defineProperty(navigator, 'hardwareConcurrency', {
-                        get: () => 8});";
-
-
-//cmdParams.Add("source", $"window.chrome = {initialScript}");
-cmdParams.Add("source", loadScript);
-driver.ExecuteCdpCommand("Page.addScriptToEvaluateOnNewDocument", cmdParams);
+if (headless)
+{
+    cmdParams.Add("source", loadScript);
+    driver.ExecuteCdpCommand("Page.addScriptToEvaluateOnNewDocument", cmdParams);
+}
 
 IJavaScriptExecutor loadChrome = (IJavaScriptExecutor)driver;
 
+//driver.Navigate().GoToUrl("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html");
+//driver.Navigate().GoToUrl("https://intoli.com/blog/making-chrome-headless-undetectable/chrome-headless-test.html");
+//string thing = driver.FindElement(By.TagName("table")).GetAttribute("innerHTML");
+//Console.WriteLine(thing);
 
-if (enableSelenium)
-{
-    driver.Navigate().GoToUrl("https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html");
-    //driver.Navigate().GoToUrl("https://intoli.com/blog/making-chrome-headless-undetectable/chrome-headless-test.html");
-    //((IJavaScriptExecutor)driver).ExecuteScript("window, 'chrome' = {runtime: {}};");
-    string thing = driver.FindElement(By.TagName("table")).GetAttribute("innerHTML");
-    Console.WriteLine(thing);
+LogIn("christian.hensman1@gmail.com", "romeo007");
+GymTrain("strength", 200);
 
-
-    //driver.Navigate().GoToUrl("https://www.torn.com/");
-    //driver.Navigate().GoToUrl("https://www.google.com/search?q=torn+city&sxsrf=ALiCzsZqGtZSjv_wU_hn-WCGoU0qA0KI8g%3A1658213089644&source=hp&ei=4VLWYuSrJIPD4-EP2b-1-Ac&iflsig=AJiK0e8AAAAAYtZg8VBsoiuG5qQNXZqLHl8t6129ARAv&ved=0ahUKEwikoKuRrYT5AhWD4TgGHdlfDX8Q4dUDCAk&uact=5&oq=torn+city&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBAgjECcyBAgjECcyCwguEIAEELEDEIMBMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIIxDqAhAnOgUIABCRAjoRCC4QgAQQsQMQgwEQxwEQ0QM6CwgAEIAEELEDEIMBOgQIABBDOggILhCABBCxAzoKCAAQsQMQgwEQQzoECC4QQzoHCAAQsQMQQzoKCAAQsQMQyQMQQzoLCC4QgAQQsQMQ1AI6BwgAEIAEEAo6CgguELEDEIMBEEM6CAgAEIAEELEDOgUILhCABFCvBFjgEWDrE2gCcAB4AIABmwKIAe4QkgEFMC41LjWYAQCgAQGwAQo&sclient=gws-wiz");
-    //ThreadRandomWait(2, 3);
-    //driver.FindElement(By.TagName("h3")).FindElement(By.XPath("./..")).Click();
-
-    //CaptchaSolver();
-
-}
+//CaptchaSolver();
 
 Console.ReadLine();
 
-static async Task Navigate(IWebDriver driver, string url)
+void LogIn(string username, string password)
 {
-    
+    //Bypasses browser check
+    DriverNavigate("https://www.torn.com/2644601");
+    ThreadRandomWait(2, 3);
+
+    //If user is not logged in
+    if (driver.Url != "https://www.torn.com/index.php")
+    {
+        driver.FindElement(By.Id("player")).SendKeys(username);
+        driver.FindElement(By.Id("password")).SendKeys(password);
+        ThreadRandomWait(7, 10);
+        driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
+    }
+}
+
+void GymTrain(string stat, int amount)
+{
+    //Check if user is already in gym
+    if (driver.Url != "https://www.torn.com/gym.php")
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("a[href*='/gym.php']")));
+        ThreadRandomWait(1, 2);
+        driver.FindElement(By.CssSelector("a[href*='/gym.php']")).Click();
+        ThreadRandomWait(1, 1.5);
+
+        //Complete captcha if there is one
+        try
+        {
+            driver.FindElement(By.Id("ui-id-3")).Click();
+            ThreadRandomWait(1, 1.5);
+            CaptchaSolver();
+            ThreadRandomWait(1, 1.5);
+            driver.FindElement(By.XPath("//input[@name='reCaptcha']")).Click();
+            ThreadRandomWait(1, 1.5);
+        } 
+        catch (Exception ex) { }
+    }
+
+    List<IWebElement> ul = driver.FindElements(By.TagName("ul")).ToList();
+    List<IWebElement> training = ul[ul.Count - 1].FindElements(By.TagName("li")).ToList();
+    string[] types = new string[] { "strength", "defense", "speed", "dexterity" };
+    //training[Array.IndexOf(types, "stat") + 1].FindElement(By.XPath($"//button[@aria-label='Train {types[Array.IndexOf(types, "stat")].ToLower()}']"));
+    training[Array.IndexOf(types, stat)].FindElement(By.TagName("input")).SendKeys(Keys.Control + "a");
+    ThreadRandomWait(0.5, 1);
+    training[Array.IndexOf(types, stat)].FindElement(By.TagName("input")).SendKeys(amount.ToString());
+    ThreadRandomWait(0.5, 1);
+    training[Array.IndexOf(types, stat)].FindElements(By.TagName("button"))[2].Click();
+    ThreadRandomWait(1, 1.5);
+}
+
+
+void DriverNavigate(string url)
+{
+    try
+    {
+        driver.Navigate().GoToUrl(url);
+    }
+    catch (Exception ex) { }
 }
 
 
@@ -152,6 +185,7 @@ void CaptchaSolver()
         ThreadRandomWait(5, 7);
         driver.FindElement(By.Id("audio-response")).SendKeys(Keys.Enter);
     }
+    driver.SwitchTo().DefaultContent();
 }
 
 string SpeechRec()
@@ -203,12 +237,12 @@ string SpeechRec()
     return (dynamic)JObject.Parse(rec.FinalResult())["text"];
 }
 
-void ImpRandomWait(int a, int b)
+void ImpRandomWait(double a, double b)
 {
     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(a + new Random().NextDouble() * 5);
 }
 
-void ThreadRandomWait(int a, int b)
+void ThreadRandomWait(double a, double b)
 {
     Thread.Sleep(Convert.ToInt32((a + new Random().NextDouble() * 5) * 1000));
 }
